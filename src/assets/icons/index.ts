769e1fa5c68cb5e7582fe8f3e08597502fc65291
Mark Learst <learst@me.com>
2025-12-12 12:15:18 -0500

    feat: add Figma-exported icon assets and update icon index
    - added 13 new Figma-exported SVG icons (c_*, icon.*, m_* prefixes)
    - included chevron, mission, and action-specific icons
    - updated icon index with new exports
    - replaced legacy icon set with Figma design system assets
    - standardized icon naming for consistency
    - improved overall icon coverage across components

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,33 +1,60 @@
 import type { FC, SVGProps } from 'react'
 
-// Import each icon as a React component using SVGR
-import ArrowLeftIcon from './ArrowLeftIcon.svg?react'
-import CameraIcon from './CameraIcon.svg?react'
-import PlayIcon from './PlayIcon.svg?react'
-import CaretIcon from './CaretIcon.svg?react'
-import RouteIcon from './RouteIcon.svg?react'
-import StopIcon from './StopIcon.svg?react'
-import XIcon from './XIcon.svg?react'
+// Import Figma icons as React components using SVGR
+import ChevronLeftIcon from './c_chevron-left.svg?react'
+import ChevronRightIcon from './c_chevron-right.svg?react'
+import ChevronDownIcon from './c_chevron-down.svg?react'
+import ChevronUpIcon from './c_chevron-up.svg?react'
+import ChevronsLeftIcon from './c_chevrons-left.svg?react'
+import ChevronsRightIcon from './c_chevrons-right.svg?react'
+import ArrowTurnDownLeftIcon from './c_arrow-turn-down-left.svg?react'
+import CameraViewfinderIcon from './c_camera-viewfinder.svg?react'
+import BinocularsIcon from './c_binoculars.svg?react'
+import ArrowDownToLineIcon from './c_arrow-down-to-line.svg?react'
+import PlayIcon from './c_play.svg?react'
+import StopSignIcon from './m_stop_sign.svg?react'
+import ArrowLeftIcon from './icon.arrow.left.svg?react'
+import OrbitIcon from './icon.orbit.iso.svg?react'
+import RouteIcon from './c_route.svg?react'
+import XSquareIcon from './x_square.svg?react'
 
 export {
-  ArrowLeftIcon,
-  CameraIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  ChevronsLeftIcon,
+  ChevronsRightIcon,
+  ArrowTurnDownLeftIcon,
+  CameraViewfinderIcon,
+  BinocularsIcon,
+  ArrowDownToLineIcon,
   PlayIcon,
-  CaretIcon,
+  StopSignIcon,
+  OrbitIcon,
   RouteIcon,
-  StopIcon,
-  XIcon,
+  ArrowLeftIcon,
+  XSquareIcon,
 }
 
 // Export a single Icons object with literal typing
 export const Icons = {
-  ArrowLeftIcon,
-  CameraIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  ChevronsLeftIcon,
+  ChevronsRightIcon,
+  ArrowTurnDownLeftIcon,
+  CameraViewfinderIcon,
+  BinocularsIcon,
+  ArrowDownToLineIcon,
   PlayIcon,
-  CaretIcon,
+  StopSignIcon,
+  OrbitIcon,
   RouteIcon,
-  StopIcon,
-  XIcon,
+  ArrowLeftIcon,
+  XSquareIcon,
 }
 
 // Derive IconName from Icons keys
